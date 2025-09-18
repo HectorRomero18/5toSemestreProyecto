3f0d6d5d4859f720ff0e6c3d8d6f8ff3d9c090da
HectorRomero18 <raulromerog002@gmail.com>
2025-09-18 16:47:49 -0500

    Reconocimiento de texto

---
+++ airwrite/domain/use_cases/list_modules.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import List, Optional
+from airwrite.application.ports.module_repository import ModuleRepositoryPort
+from airwrite.domain.entities.module import ModuleEntity
+
+
+@dataclass
+class ListModulesQuery:
+    q: Optional[str] = None
+
+
+class ListModulesUseCase:
+    def __init__(self, repo: ModuleRepositoryPort) -> None:
+        self._repo = repo
+
+    def execute(self, query: ListModulesQuery) -> List[ModuleEntity]:
+        return self._repo.list(q=query.q)
