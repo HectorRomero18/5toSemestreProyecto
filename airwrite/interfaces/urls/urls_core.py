8721e79a6c5a62acb470f7ee5f298d8331c40933
HectorRomero18 <raulromerog002@gmail.com>
2025-10-30 22:58:43 -0500

    Funcionalidad de cambio de color y grosor aplicada

--- airwrite/interfaces/urls/urls_core.py
+++ airwrite/interfaces/urls/urls_core.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from airwrite.interfaces.django_views.auth import LoginView, CustomLogoutView
 from airwrite.interfaces.django_views.home import ModuleListView
-from airwrite.interfaces.django_views.trazos import index, video_feed_cam, video_feed_canvas, clear_canvas
+from airwrite.interfaces.django_views.trazos import index, video_feed_cam, video_feed_canvas, clear_canvas, set_color, set_grosor
 #from core.views.login import login_view
 
 urlpatterns = [
@@ -24,6 +24,9 @@ urlpatterns = [
     path('video/cam/', video_feed_cam, name='video_feed_cam'),
     path('video/canvas/', video_feed_canvas, name='video_feed_canvas'),
     path('clear/', clear_canvas, name='clear_canvas'),
+    path('set_grosor/', set_grosor, name='set_grosor'),
+
+    path('color/', set_color, name='set_color'),
 
     
 ]
