8e421380cd289e82c0b08b7d16e4511b222ff191
HectorRomero18 <raulromerog002@gmail.com>
2025-10-31 23:42:22 -0500

    Agregando la funcionalidad de comprar letras (Version 0.10)

--- opencv/urls.py
+++ opencv/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 # from django.http import HttpResponse
 
 
@@ -31,5 +33,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
